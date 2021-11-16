9c1f326cda10e58beb7186cfb12ece1d3829d3c7
Lakitha Madanayake <lakitha1999@gmail.com>
2021-11-16 16:56:41 +0530

    Create the login UI & firebase

---
+++ demoapp/lib/screens/home_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Welcome"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 150,
+                child: Image.asset("assets/its.png", fit: BoxFit.contain),
+              ),
+              Text(
+                "Welcome Back",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              RaisedButton(
+                  child: Text("Logout",style: TextStyle(color: Colors.white),),
+                  color: Colors.blueAccent,
+                  onPressed: () {
+                    logout(context);
+                  }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // the logout function
+  Future<void> logout(BuildContext context) async {
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => LoginScreen()));
+  }
+}
